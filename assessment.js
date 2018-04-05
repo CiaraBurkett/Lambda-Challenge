32334cf562e2aeab73544a5c424ba551a621a104
Ciara Burkett <ciaraburkett@gmail.com>
2018-04-05 09:41:46 -0400

    Completed assessment

--- assessment.js
+++ assessment.js
@@ -7,7 +7,7 @@ After writing your function uncomment the matching function reference at the bot
 // 1. Write a function called helloWorld that returns the string 'Hello World!'.
 
 function helloWorld() {
-
+  return "Hello World!";
 }
 
 /*
@@ -23,8 +23,19 @@ function helloWorld() {
              lambdaSchool(8); // returns 8
 */
 
-function lambdaSchool() {
-
+function lambdaSchool(num) {
+  if (num % 15 == 0) {
+    return "Lambda School";
+  }
+  else if (num % 5 == 0) {
+    return "School";
+  }
+  else if (num % 3 == 0) {
+    return "Lambda";
+  }
+  else {
+    return num;
+  }
 }
 
 /*
@@ -38,8 +49,12 @@ function lambdaSchool() {
              longestString(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
 */
 
-function longestString() {
+function longestString(strs) {
+  strs.sort(function(a, b) {
+    return b.length - a.length;
+  });
 
+  return strs[0];
 }
 
 /*
@@ -63,8 +78,16 @@ function longestString() {
              computeUserAverageAge(users); // returns 62 (This number is rounded up from 61.6666)
 */
 
-function computeUserAverageAge() {
+function computeUserAverageAge(users) {
+  var userAges = [];
 
+  users.forEach(function(element) {
+    userAges.push(element.age);
+  });
+
+  var reducer = (accumulator, currentValue) => accumulator + currentValue;
+  
+  return Math.round(userAges.reduce(reducer)/userAges.length);
 }
 
 module.exports = {
